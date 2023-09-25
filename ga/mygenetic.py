e92e2c7f136519a04c1f0bd8ca1d07aac54ed037
Yuri Garcia Ribeiro <yurigarcia1008@hotmail.com>
2023-09-24 22:42:41 -0300

    refatoracao

--- ga/mygenetic.py
+++ ga/mygenetic.py
@@ -27,115 +27,85 @@ class MyGeneticAlgorithm(Algorithm):
         self.all_ids = all_ids
         self.query_search = query_search
         
+        self.lista_filmes_usuario_asistiu =  RatingsRepository.find_by_userid(self.db, self.query_search)
+        print(self.lista_filmes_usuario_asistiu)
+        ratings_usuario = RatingsRepository.find_by_userid(self.db, self.query_search)
+        self.media_nota_filmes_usuario_asistiu = sum([
+            x.rating for x in ratings_usuario
+        ]) / len(ratings_usuario)
 
-    
+        self.media_ano_filmes_usuario_asistiu = np.mean([MovieRepository.find_by_id(self.db, x.movieId).year for x in self.lista_filmes_usuario_asistiu])
+
+
+        # Pega os gêneros dos filmes assistidos pelo usuário
+        generos = [MovieRepository.find_by_id(self.db, x.movieId).genres for x in self.lista_filmes_usuario_asistiu]
+        # Divide os gêneros e cria uma lista única
+        generos_split = [g for sublist in generos for g in sublist.split('|')]
+        # Conta as ocorrências de cada gênero
+        contagem_generos = {}
+        for gen in generos_split:
+            contagem_generos[gen] = contagem_generos.get(gen, 0) + 1
+        # Pega os três gêneros mais comuns
+        sorted_generos = sorted(contagem_generos.keys(), key=lambda x: contagem_generos[x], reverse=True)
+        self.lista_generos_usuario = sorted_generos[:3]
+
+
+        
+
     def evaluate(self, individual):
-
-        #self.query_search codigo da pessoa
-        #individual lista de codigo dos filmes
-
+        
+        """
+        Avalia a adequação (fitness) de um indivíduo com base em critérios de filmes.
+        
+        :param individual: Lista de IDs de filmes representando o indivíduo.
+        :return: Uma tupla contendo o valor de adequação (fitness).
+        """
+        
+        # Certifica-se de que o indivíduo não tem IDs repetidos e pertence ao conjunto de todos os IDs.
         if len(individual) != len(set(individual)):
             return (0.0, )
-        
         if len(list(set(individual) - set(self.all_ids))) > 0:
             return (0.0, )
-        
-        ratings_movies_user = RatingsRepository.find_by_userid(self.db, self.query_search)
-        ratings_movies = RatingsRepository.find_by_movieid_list(self.db, individual)
 
-        if len(ratings_movies_user) == 0:
-            return (0.0, )
-        
-        if len(ratings_movies) == 0:
-            return (0.0, )
-        
-        
-        #pega os generos mais assistidos pelo usuario
-        genres_user = []
-        for movie in ratings_movies_user:
-            genres_user.append(MovieRepository.find_by_id(self.db, movie.movieId).genres)
+        # Definição de pesos para critérios de avaliação
+        YEAR_INTERVAL = 5
+        PESO_GENERO = 3
+        PESO_NOTA = 2
+        PESO_ANO = 1
 
-        genres_user = [item for sublist in genres_user for item in sublist.split('|')]
+        # Consulta filmes do banco de dados com base nos IDs fornecidos
+        movies_from_db = {movie_id: MovieRepository.find_by_id(self.db, movie_id) for movie_id in individual}
 
-        #pega os 3 generos mais assistidos pelo usuario
-        genres_user = list(dict.fromkeys(genres_user))
-        genres_user = genres_user[:3]
-        
+        # Determina os filmes do indivíduo que o usuário ainda não assistiu
+        filmes_nao_assistidos = list(set(individual) - set(self.lista_filmes_usuario_asistiu))
 
-        #pega os filmes que tenham os generos mais assistidos pelo usuario
-        genres_user_set = set(genres_user)
-        movies_user = []
+        # Calcula médias dos ratings dos filmes não assistidos
+        media_filmes = {}
+        ratings_dos_filmes = RatingsRepository.find_by_movieid_list(self.db, filmes_nao_assistidos)
+        for filme_id in filmes_nao_assistidos:
+            ratings_do_filme = [rating for rating in ratings_dos_filmes if rating.movieId == filme_id]
+            if ratings_do_filme:
+                media_do_filme = sum([rating.rating for rating in ratings_do_filme]) / len(ratings_do_filme)
+                media_filmes[filme_id] = media_do_filme
 
-        for movie in ratings_movies:
-            genres_movie = set(MovieRepository.find_by_id(self.db, movie.movieId).genres.split('|'))
-            if genres_movie & genres_user_set:  # Verifica interseção de conjuntos (se têm gêneros em comum)
-                movies_user.append(movie.movieId)
+        # Filmes cuja média de avaliação é superior à média do usuário
+        filmes_acima_da_media_usuario = [filme_id for filme_id, media in media_filmes.items() if media > self.media_nota_filmes_usuario_asistiu]
 
-        
-        #dos filmes que tem os generos mais assistidos pelo usuario, pega os que o usuario ainda nao assistiu
-        movies_user = list(set(movies_user) - set([movie.movieId for movie in ratings_movies_user]))
+        # Filmes que correspondem ao intervalo de anos preferido
+        filmes_intervalo_anos = [
+            filme_id for filme_id in filmes_nao_assistidos 
+            if movies_from_db[filme_id].year is not None and 
+            self.media_ano_filmes_usuario_asistiu - YEAR_INTERVAL <= movies_from_db[filme_id].year <= self.media_ano_filmes_usuario_asistiu + YEAR_INTERVAL
+        ]
 
-        #pega os filmes com as maiores notas dos generos mais assistidos pelo usuario
-        movies_user_not_watched = []
-        movies_user_not_watched = RatingsRepository.find_by_movieid_list(self.db, movies_user)
-        movies_user_not_watched.sort(key=lambda x: x.rating, reverse=True)
+        # Filmes que se encaixam nos gêneros preferidos do usuário
+        filmes_genero_preferido = [filme for filme in filmes_nao_assistidos if any(genero in self.lista_generos_usuario for genero in movies_from_db[filme].genres.split('|'))]
 
-        #pega a nota media para cada filmes que o usuario ainda nao assistiu
-        movies_user_not_watched_mean = [
-                {
-                    'movieId': movie.movieId,
-                    'mean': np.mean([obj_.rating for obj_ in RatingsRepository.find_by_movieid(self.db, movie.movieId)])
-                }
-                for movie in movies_user_not_watched
-            ]
-
-        #pega a nota media dos filmes que o usuario ja assistiu
-        mean_user = 0.0
-        if len(ratings_movies_user) > 0:
-            mean_user = np.mean([obj_.rating for obj_ in ratings_movies_user])
-        else:
-            mean_user = 0.0
-        
-
-        #pega todos os filmes que tenham a media maior que a media do usuario
-        movies_user_not_watched_mean = [movie for movie in movies_user_not_watched_mean if movie['mean'] > mean_user]
-
-        #mega a media dos filmes que tenham a media maior que a media do usuario
-        mean_movies_user_not_watched_mean = 0.0
-        if len(movies_user_not_watched_mean) > 0:
-            mean_movies_user_not_watched_mean = np.mean([obj_['mean'] for obj_ in movies_user_not_watched_mean])
-        else:
-            mean_movies_user_not_watched_mean = 0.0
-
-
-        #pega o ano de lancamento dos filmes que o usuario ja assistiu
-        years_user = [MovieRepository.find_by_id(self.db, movie.movieId).year for movie in ratings_movies_user]
-
-
-        #pega a media dos anos de lancamento dos filmes que o usuario ja assistiu
-        mean_years_user = 0.0
-        if len(years_user) > 0:
-            mean_years_user = np.mean(years_user)
-        else:
-            mean_years_user = 0.0
-
-        #pega todos os filmes que tenham o ano de lançamento proximo da media dos anos de lancamento dos filmes que o usuario ja assistiu (5 anos de diferença para maimovies_user_not_watched_mean s ou para menos))) 
-        movies_years_user_not_watched_mean = []
-        for movie in movies_user_not_watched:
-            if  mean_user + 5 > abs(MovieRepository.find_by_id(self.db, movie.movieId).year) > mean_years_user - 5:
-                movies_years_user_not_watched_mean.append(movie.movieId)
-
-        fitness = 0.0
-        fitness += mean_movies_user_not_watched_mean * 0.3 #nota media dos filmes que o usuario nao assistiu e tem media maior que a media do usuario
-        fitness += len(movies_user_not_watched_mean) * 0.1#numero de filmes que o usuario nao assistiu e tem media maior que a media do usuario
-        fitness += mean_user * 0.2 #nota media dos filmes que o usuario ja assistiu
-        fitness += mean_years_user * 0.1#media dos anos de lancamento dos filmes que o usuario ja assistiu
-        fitness += len(movies_years_user_not_watched_mean) * 0.3 #numero de filmes que o usuario nao assistiu e tem media maior que a media do usuario e tem o ano de lancamento proximo da media dos anos de lancamento dos filmes que o usuario ja assistiu (5 anos de diferença para mais ou para menos)))
-
+        # Cálculo final da adequação (fitness)
+        fitness = len(filmes_acima_da_media_usuario) * PESO_NOTA + len(filmes_intervalo_anos) * PESO_ANO + len(filmes_genero_preferido) * PESO_GENERO
 
         print(fitness)
-
-        #media final igual a nota media dos filmes q o usuario nao assistiu e tem media maior que a media do usuario + numero de filmes que o usuario nao assistiu e tem media maior que a media do usuario + nota media dos filmes que o usuario ja assistiu
+        
         return (fitness,  )
         
 
